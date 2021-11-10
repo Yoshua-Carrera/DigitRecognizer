6cf7fb1589af8f819e5f07577c4df0b7b8af26ef
Yoshua J Carrera <yoshua.carrera24@gmail.com>
2021-11-09 20:39:22 -0500

    Fixed NN Model

--- nn.py
+++ nn.py
@@ -13,16 +13,18 @@ x_train, x_test, y_train, y_test = dataTool.split_data(train, 'label', True, 0.4
 
 nn = Sequential()
 
-nn.add(Dense(10, input_dim=784, activation='relu'))
+nn.add(Dense(784, input_dim=784, activation='relu'))
 nn.add(Dense(50, activation='relu'))
 nn.add(Dense(10, activation='relu'))
 nn.add(Dense(5, activation='relu'))
-nn.add(Dense(10, activation='sigmoid'))
+nn.add(Dense(1, activation='sigmoid'))
 
 nn.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 nn.summary()
 
-history = nn.fit(x_train, y_train, epochs=100, batch_size=10)
+x_train_array = np.asarray(x_train)
+
+history = nn.fit(x_train_array, y_train, epochs=100, batch_size=10)
 
 plt.plot(history.epoch, history.history['loss'])
 plt.xlabel('Iteration')
