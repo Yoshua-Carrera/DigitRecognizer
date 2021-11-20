1e3c5ed9fe6cb12351c63c691fc9f3e298174e08
Yoshua J Carrera <yoshua.carrera24@gmail.com>
2021-11-20 18:50:46 -0500

    Added prediction script

--- nn.py
+++ nn.py
@@ -8,62 +8,69 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-
 from dataImportCleaning import dataTool
 
-dataTool = dataTool('data/train.csv', 'data/test.csv')
+def nn_prediction(showEval: bool, epochs: int, splt: bool, testSize: float=.25):
+    data_Tool = dataTool('data/train.csv', 'data/test.csv')
+    train, test =  data_Tool.retrieveData
 
-train, test =  dataTool.retrieveData
+    if splt:
+        x_train, x_test, y_train, y_test = data_Tool.split_data(train, 'label', splt, testSize)
+        x_train, x_test = data_Tool.normalize(train.drop('label', axis = 1), [x_train, x_test])
+    else:
+        x_train, y_train = data_Tool.split_data(train, 'label', splt, testSize)
+        x_train = data_Tool.normalize(train.drop('label', axis = 1), [x_train])
 
-x_train, x_test, y_train, y_test = dataTool.split_data(train, 'label', True, 0.4)
+    # add neural network model
+    nn = Sequential()
 
-x_train, x_test = dataTool.normalize(train.drop('label', axis = 1), [x_train, x_test])
+    '''
+    first layer is input layer and has 784 inputs and relu activation
+    second layer has 128 neurons and also relu activation
+    third layer has 128 neurons and also relu activation
+    fourth layer has 10 output neurons, it's one for each digit and it's softmax activation
 
-# add neural network model
-nn = Sequential()
+    '''
+    nn.add(Dense(10, input_dim=784, activation='relu'))
+    nn.add(Dense(128, activation='relu'))
+    nn.add(Dense(128, activation='relu'))
+    nn.add(Dense(10, activation='softmax'))
 
-'''
-first layer is input layer and has 784 inputs and relu activation
-second layer has 128 neurons and also relu activation
-third layer has 128 neurons and also relu activation
-fourth layer has 10 output neurons, it's one for each digit and it's softmax activation
+    nn.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    nn.summary()
 
-'''
-nn.add(Dense(10, input_dim=784, activation='relu'))
-nn.add(Dense(128, activation='relu'))
-nn.add(Dense(128, activation='relu'))
-nn.add(Dense(10, activation='softmax'))
-
-nn.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-nn.summary()
-
-x_train_array = np.asarray(x_train)
-
-history = nn.fit(x_train_array, y_train, epochs=100, batch_size=10)
-
-plt.plot(history.epoch, history.history['loss'])
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
-plt.show()
-
-plt.plot(history.epoch, history.history['accuracy'])
-plt.xlabel('Iteration')
-plt.ylabel('Accuracy')
-plt.show()
-
-y_pred = nn.predict(x_test)
-
-
-loss,accuracy = nn.evaluate(x_test,y_test)
-print(loss,accuracy)
-
-
-prediction = nn.predict(x_test)
-actual_prediction = []
-
-for item in prediction:
-    actual_prediction.append(np.argmax(item))
-
-
-print(f'Accuracy: {accuracy_score(actual_prediction, y_test)}')
+    # x_train_array = np.asarray(x_train)
     
+    history = nn.fit(x_train, y_train, epochs=epochs, batch_size=10)
+
+    prediction = nn.predict(test)
+    actual_prediction = []
+
+    for item in prediction:
+        actual_prediction.append(np.argmax(item))
+    
+    if splt:
+        print(f'Accuracy: {accuracy_score(actual_prediction, y_test)}')
+    
+    if showEval:
+        plt.plot(history.epoch, history.history['loss'])
+        plt.xlabel('Iteration')
+        plt.ylabel('Loss')
+        plt.show()
+
+        plt.plot(history.epoch, history.history['accuracy'])
+        plt.xlabel('Iteration')
+        plt.ylabel('Accuracy')
+        plt.show()
+
+        y_pred = nn.predict(x_test)
+
+        loss,accuracy = nn.evaluate(x_test,y_test)
+        print(loss,accuracy)
+        
+        return actual_prediction
+    else:
+        print(data_Tool.brr)
+
+        return actual_prediction
+        
---
+++ prediction.py
@@ -0,0 +1,17 @@
+import pathlib
+import pandas as pd
+
+from nn import nn_prediction
+
+pathlib.Path('prediction').mkdir(parents=True, exist_ok=True)
+
+prediction = nn_prediction(showEval=False, epochs=100, splt=False)
+
+submission = {
+    'ImageID': range(len(prediction)),
+    '# Label': prediction    
+}
+
+submission_df = pd.DataFrame(submission)
+
+submission_df.to_csv('prediction/submission.csv', index=False)
